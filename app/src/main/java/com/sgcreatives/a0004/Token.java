1b5c739022f213261a24254dcade82eb1e18cc56
jaisalsabu <jaisal1010sabu@gmail.com>
2020-01-13 15:41:03 +0530

    retriving

--- app/src/main/java/com/sgcreatives/a0004/Token.java
+++ app/src/main/java/com/sgcreatives/a0004/Token.java
@@ -1,6 +1,7 @@
 package com.sgcreatives.a0004;
 
 
+import android.content.SharedPreferences;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -16,13 +17,29 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Token extends AppCompatActivity {
         Button btnCreate,btnload;
         EditText txttox;
+        String name;
+        SharedPreferences sharedPreferencess;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +48,56 @@ public class Token extends AppCompatActivity {
             txttox = findViewById(R.id.editText);
             btnload=findViewById(R.id.button);
             btnCreate = (Button) findViewById(R.id.button2);
+            btnload.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                            name = sharedPreferencess.getString("name", "");
+                            StringRequest stringRequest = new StringRequest(Request.Method.POST, "",
+                                    new Response.Listener<String>() {
+                                        @Override
+                                        public void onResponse(String response) {
+//If we are getting success from server
+
+                                            Toast.makeText(Token.this, response, Toast.LENGTH_LONG).show();
+                                            try {
+                                                JSONArray jsonArray = new JSONArray(response);
+                                                for (int i = 0; i < jsonArray.length(); i++) {
+                                                    JSONObject json_obj = jsonArray.getJSONObject(i);
+
+                                                }
+                                            } catch (JSONException e) {
+                                                e.printStackTrace();
+                                            }
+
+                                        }
+                                    },
+                                    new Response.ErrorListener() {
+                                        @Override
+                                        public void onErrorResponse(VolleyError error) {
+//You can handle error here if you want
+                                        }
+
+                                    }) {
+                                @Override
+                                protected Map<String, String> getParams() throws AuthFailureError {
+                                    Map<String, String> params = new HashMap<>();
+//Adding parameters to request
+
+                                    params.put("find",name);
+
+//returning parameter
+                                    return params;
+                                }
+                            };
+
+//Adding the string request to the queue
+                            RequestQueue requestQueue = Volley.newRequestQueue(Token.this);
+                            requestQueue.add(stringRequest);
+
+
+                        }
+                    });
+                
             btnCreate.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
